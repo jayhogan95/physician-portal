a6eb055466a8fa9c387d8674b39dceca96b123e6
Jay Hogan <jayhogan95@gmail.com>
2020-11-04 22:02:41 +0000

    middleware added

---
+++ middleware/index.js
@@ -0,0 +1,19 @@
+const middlewareObj = {};
+
+middlewareObj.isLoggedIn = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	req.flash("error", "Please login first!");
+	res.redirect("/login");
+};
+
+middlewareObj.isAdmin = (req, res, next) => {
+	if (req.user.isAdmin) {
+		return next();
+	}
+	req.flash("error", "You do not have permission to do that!");
+	res.redirect("/products");
+};
+
+module.exports = middlewareObj;
