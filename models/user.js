9b55462921daa63571fbbce86619d8957d60817b
Jay Hogan <jayhogan95@gmail.com>
2020-11-05 18:24:10 +0000

    added new user button on admin page and made minor adjustments to profile page

--- models/user.js
+++ models/user.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
-const defaultURL = "/images/health_complex_icon_small.jpg";
 
 const UserSchema = new mongoose.Schema({
 	username: {type: String, unique: true, required: true},
 	password: String,
-	avatar: {type: String, default: defaultURL},
+	avatar: String,
 	firstName: String,
 	lastName: String,
 	email: {type: String, unique: true, required: true},
