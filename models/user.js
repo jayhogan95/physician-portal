7ac4393b6f965353dd2863ae69de3adbb3d306e5
Jay Hogan <jayhogan95@gmail.com>
2020-12-28 19:52:34 +0000

    chnaged orders on landing page to search orders

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
 	email: {type: String, unique: true, required: true},
 	resetPasswordToken: String,
 	resetPasswordExpires: Date,
-	isAdmin: {type: Boolean, default: true},
+	isAdmin: {type: Boolean, default: false},
 	role: {type: String, enum: ["Employee", "Physician"]},
 	branchAssoc: [String]
 });
